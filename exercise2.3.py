45228800c12131a8e892e12d4a21fecd02113f0e
billygl <billy.grados@gmail.com>
2022-05-24 08:13:18 -0500

    feat: solve 3

--- exercise2.3.py
+++ exercise2.3.py
@@ -5,3 +5,11 @@ for i in range(10):
     print('\r' + str(seconds), end='', flush=True)
     seconds = seconds + 1
     time.sleep(1)
+
+def timer(seconds, end):
+    if seconds < end:
+        print('\r' + str(seconds), end='', flush=True)
+        time.sleep(1)
+        return timer(seconds + 1, end)
+
+timer(0, 10)
