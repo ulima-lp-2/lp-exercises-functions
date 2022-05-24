ccf69ebbc21bfa486d199aed661ba1e873895039
billygl <billy.grados@gmail.com>
2022-05-24 07:52:38 -0500

    feat: solve 1 - part 1

--- exercise2.1.py
+++ exercise2.1.py
@@ -1,17 +1,21 @@
+def bmi_calc(weight_kg, height_m):
+    bmi = int(weight_kg / height_m**2)
+    return bmi
+
 # Subject data = [weight_kg, height_m]
 subject1 = [80, 1.62]
 subject2 = [69, 1.53]
 subject3 = [80, 1.66]
 subject4 = [80, 1.79]
 
-bmi_subject1 = int(subject1[0] / subject1[1]**2)
+bmi_subject1 = bmi_calc(subject1[0], subject1[1])
 print("bmi {} = {}".format('subject1', bmi_subject1))
 
-bmi_subject2 = int(subject2[0] / subject2[1]**2)
+bmi_subject2 = bmi_calc(subject2[0], subject2[1])
 print("bmi {} = {}".format('subject2', bmi_subject2))
 
-bmi_subject3 = int(subject3[0] / subject3[1]**2)
+bmi_subject3 = bmi_calc(subject3[0], subject3[1])
 print("bmi {} = {}".format('subject3', bmi_subject3))
 
-bmi_subject4 = int(subject4[0] / subject4[1]**2)
+bmi_subject4 = bmi_calc(subject4[0], subject4[1])
 print("bmi {} = {}".format('subject4', bmi_subject4))
