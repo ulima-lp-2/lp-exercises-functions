f7c85980137c7a69cac04d3cde2177c9421543ab
billygl <billy.grados@gmail.com>
2022-05-24 07:57:36 -0500

    feat: solve 1 - part 3

--- exercise2.1.py
+++ exercise2.1.py
@@ -4,12 +4,12 @@ def bmi_calc(sub_num, weight_kg, height_m):
     print("bmi {} = {}".format(subject, bmi))
 
 # Subject data = [weight_kg, height_m]
-subject1 = [80, 1.62]
-subject2 = [69, 1.53]
-subject3 = [80, 1.66]
-subject4 = [80, 1.79]
+subjects = [
+    [1, 80, 1.62],
+    [2, 69, 1.53],
+    [3, 80, 1.66],
+    [4, 80, 1.79]
+]
 
-bmi_subject1 = bmi_calc(1, subject1[0], subject1[1])
-bmi_subject2 = bmi_calc(2, subject2[0], subject2[1])
-bmi_subject3 = bmi_calc(3, subject3[0], subject3[1])
-bmi_subject4 = bmi_calc(4, subject4[0], subject4[1])
+for sub in subjects:
+    bmi_calc(sub[0], sub[1], sub[2])
