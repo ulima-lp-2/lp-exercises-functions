1c23691fe1b4a4f0ebecc3d66b77f979fd41476e
billygl <billy.grados@gmail.com>
2022-05-24 08:55:11 -0500

    feat: solve 4

--- exercise2.4.py
+++ exercise2.4.py
@@ -3,5 +3,18 @@ import random
 
 def getRandom():
     r = random.randint(1,9)
-    # print(r)
+    #print(r)
     return r
+
+print('i\tend\tn\tsum')
+def reduce(i, end, sum):
+    n = getRandom()
+    print(i, '\t', end, '\t', n, '\t', sum)
+    if i < end:
+        sum_ = sum + n
+        #print('sum:', sum_)
+        return reduce(i + 1, end, sum_)
+    else:
+        return sum
+
+print('suma: %d' % reduce(0, 5, 0))
