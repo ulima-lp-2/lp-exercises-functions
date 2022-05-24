5150bf0ca5a4c737dcf7249f79e7581c30e6780b
billygl <billy.grados@gmail.com>
2022-05-24 07:54:57 -0500

    feat: solve 1 - part 2

--- exercise2.1.py
+++ exercise2.1.py
@@ -1,6 +1,7 @@
-def bmi_calc(weight_kg, height_m):
+def bmi_calc(sub_num, weight_kg, height_m):    
+    subject = 'subject' + str(sub_num)
     bmi = int(weight_kg / height_m**2)
-    return bmi
+    print("bmi {} = {}".format(subject, bmi))
 
 # Subject data = [weight_kg, height_m]
 subject1 = [80, 1.62]
@@ -8,14 +9,7 @@ subject2 = [69, 1.53]
 subject3 = [80, 1.66]
 subject4 = [80, 1.79]
 
-bmi_subject1 = bmi_calc(subject1[0], subject1[1])
-print("bmi {} = {}".format('subject1', bmi_subject1))
-
-bmi_subject2 = bmi_calc(subject2[0], subject2[1])
-print("bmi {} = {}".format('subject2', bmi_subject2))
-
-bmi_subject3 = bmi_calc(subject3[0], subject3[1])
-print("bmi {} = {}".format('subject3', bmi_subject3))
-
-bmi_subject4 = bmi_calc(subject4[0], subject4[1])
-print("bmi {} = {}".format('subject4', bmi_subject4))
+bmi_subject1 = bmi_calc(1, subject1[0], subject1[1])
+bmi_subject2 = bmi_calc(2, subject2[0], subject2[1])
+bmi_subject3 = bmi_calc(3, subject3[0], subject3[1])
+bmi_subject4 = bmi_calc(4, subject4[0], subject4[1])
